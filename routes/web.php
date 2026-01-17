e4ee892d307c3f0fea55968d791b37d8ef076052
houssamBensiyed <med.ab02017@gmail.com>
2026-01-17 18:19:39 +0100

    Feat: add basic public routes

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 
-// 
+/*
+|--------------------------------------------------------------------------
+| Part 1: Basic Public Routes
+|--------------------------------------------------------------------------
+*/
+
+// View Route for homepage
+Route::view('/', 'home')->name('home');
+
+// View route with Data passed to the view
+Route::view('/contact', 'contact', ['phone' => '555-1234'])->name('contact');
+
+// Permanent Redirect 301
+Route::redirect('/food-menu', '/menu', 301);
+
+// Temporary Redirect 302
+Route::redirect('/promo', '/menu/specials', 302);
